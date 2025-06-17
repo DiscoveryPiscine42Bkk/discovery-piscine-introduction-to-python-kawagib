e470b7334e3eab664a0359287645d7eaa42686b2
kawagib <68030207@kmitl.ac.th>
2025-06-17 07:58:19 +0000

    add round_up.py

--- cell04/ex03/float.py
+++ cell04/ex03/float.py
@@ -3,11 +3,11 @@ def main():
     try:
         number = float(user_input)
         if number.is_integer():
-            print("this number is an integer.")
+            print("This number is an integer.")
         else:
             print("This number is a decimal.")
     except ValueError:
-        print("Invalid input.")
+            print("Invalid input.")
 
 if __name__ == "__main__":
     main()
