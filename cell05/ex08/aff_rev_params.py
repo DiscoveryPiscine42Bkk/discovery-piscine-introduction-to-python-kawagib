f701175bd89d839314301b50c692014d7dee399b
kawagib <68030207@kmitl.ac.th>
2025-06-18 03:52:13 +0000

    add aff_rev_params.py

---
+++ cell05/ex08/aff_rev_params.py
@@ -0,0 +1,11 @@
+import sys
+
+def main():
+    if len(sys.argv) < 3:
+        print("none")
+    else:
+        for param in reversed(sys.argv[1:]):
+            print(param)
+
+if __name__ == "__main__":
+    main()
