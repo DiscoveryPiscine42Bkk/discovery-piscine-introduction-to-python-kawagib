0273ce16635c9211875f4cd181c15dd44766f3a8
kawagib <68030207@kmitl.ac.th>
2025-06-18 03:58:28 +0000

    add scan_it.py

---
+++ cell05/ex09/scan_it.py
@@ -0,0 +1,20 @@
+import Sys
+import re
+
+def main():
+    if len(sys.argv) < 3:
+        print("none")
+        return
+    
+    keyword = sys.argv[1]
+    text = sys.argv[2]
+
+    matches = re.findall(re.escape(keyword), text)
+
+    if matches:
+        print(len(matches))
+    else:
+        print("none")
+
+if __name__ == "__main__":
+    main()
