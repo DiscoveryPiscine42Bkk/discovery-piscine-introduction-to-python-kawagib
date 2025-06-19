e78528bfbdf3d086c5a2e6da88109bbd158ef976
kawagib <68030207@kmitl.ac.th>
2025-06-19 03:03:52 +0000

    add main.py

--- Rush00/ex00/main.py
+++ Rush00/ex00/main.py
@@ -1,12 +1,21 @@
+import sys
 from checkmate import checkmate
 
+def read_board_from_file(filename):
+    try:
+        with open(filename, 'r') as f:
+            content = f.read()
+        return content
+    except:
+        return None
+
 def main():
-    board = """\
-R...
-.K..
-..P.
-...."""
-    checkmate(board)
+    for filename in sys.argv[1:]:
+        board = read_board_from_file(filename)
+        if board is None:
+            print("Error")
+            continue
+        checkmate(board)
 
 if __name__ == "__main__":
     main()
